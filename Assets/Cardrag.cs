4563f0a9e10acda709f82593b40d079b62af35d4
hoangGTVT <dungvetre08@gmail.com>
2025-03-10 21:21:51 +0700

    ok

--- Assets/Cardrag.cs
+++ Assets/Cardrag.cs
@@ -5,6 +5,7 @@ using UnityEngine.EventSystems;
 public class Cardrag : MonoBehaviour,IBeginDragHandler, IEndDragHandler,IDragHandler
 {
     public Transform mparent=null;
+    public CardEff cardEff;
     public void OnBeginDrag(PointerEventData eventData)
     {
         mparent = this.transform.parent;
@@ -14,13 +15,21 @@ public class Cardrag : MonoBehaviour,IBeginDragHandler, IEndDragHandler,IDragHan
     public void OnEndDrag(PointerEventData eventData)
     {
         this.transform.SetParent(mparent);
+        this.transform.position=mparent.position;
         GetComponent<CanvasGroup>().blocksRaycasts = true;
-
+        this.transform.localScale=Vector3.one;
+        cardEff.ActiveOutline(true);
+        cardEff.ChangeColor(cardEff.ColorIdle);
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         this.transform.position = eventData.position;
 
+    }
+    private void Start()
+    {
+        cardEff.ActiveOutline(true);
+        cardEff.ChangeColor(cardEff.ColorAction);
     }
 }
