d4aac9675ea18edf7dbdadcb7a6b44f06c04228c
HoangTeamDev <dungvetre08@gmail.com>
2025-03-18 10:56:01 +0700

    aaaaaaa

---
+++ Assets/Scripts/Connet/GameServer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public partial class GameServer : MonoBehaviourPun
+
+{
+    public static GameServer Instance;
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            PlayCard("Rồng Thần");
+        }
+    }
+}
