368f54940be4faf00ad1a701b11eb34efc82d790
hoangGTVT <dungvetre08@gmail.com>
2025-03-26 22:38:13 +0700

    vcx

---
+++ Assets/Scripts/Library/CardInventory.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardInventory : MonoBehaviour
+{
+    public int idCard;
+    public Image image;
+    public TextMeshProUGUI textMeshProUGUI;
+    public Button ButtonInfo;
+   
+}
