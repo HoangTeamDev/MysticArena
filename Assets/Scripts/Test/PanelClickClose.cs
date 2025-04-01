17c101bdb270c51ea9f190f2ac32a9266a1d1f61
hoangGTVT <dungvetre08@gmail.com>
2025-04-01 21:33:14 +0700

    cccc

--- Assets/Scripts/Test/PanelClickClose.cs
+++ Assets/Scripts/Test/PanelClickClose.cs
@@ -1,15 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class PanelClickCloser : MonoBehaviour, IPointerClickHandler
+public class PanelClickCloser : MonoBehaviour, IPointerClickHandler, ISubmitHandler
 {
     public void OnPointerClick(PointerEventData eventData)
     {
-        ClosePanel();
+        if (eventData.pointerCurrentRaycast.gameObject == this.gameObject)
+        {
+            Debug.Log("Click vào PANEL");
+            ClosePanel();
+        }
+       
+        
     }
-
+    public void OnSubmit(BaseEventData panelClickCloser)
+    {
+        Debug.Log("Click vào PANEL1");
+    }
     void ClosePanel()
     {
         gameObject.SetActive(false); // hoặc animation, hoặc bất cứ xử lý gì
