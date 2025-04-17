4f7497d2417ea408dd96ea609eafd6a4f45d77a1
HoangTeamDev <dungvetre08@gmail.com>
2025-04-17 08:05:03 +0700

    up

---
+++ Assets/Scripts/UI/UIWindow/Shop.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UI.ItemUI;
+using UI.SystemUI;
+using UnityEngine;
+using UnityEngine.EventSystems;
+namespace UI.UIWindow
+{
+    public class Shop : UIBase
+    {
+        
+        public override void Init()
+        {
+            base.Init();
+        }
+
+        public override void OnPointerClick(PointerEventData pointerEventData)
+        {
+            base.OnPointerClick(pointerEventData);
+        }
+
+        public override void Open()
+        {
+            base.Open();
+        }
+
+        public override void OpenMe()
+        {
+            base.OpenMe();
+        }
+
+        public override void ShowInfoItem(StringBuilder sb, Item item)
+        {
+            base.ShowInfoItem(sb, item);
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+        }
+        public override void Close()
+        {
+            base.Close();
+        }
+
+        public override void CloseMe()
+        {
+            base.CloseMe();
+        }
+
+    }
+}
+
